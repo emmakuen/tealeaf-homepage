c0196d45749cdefeca60047a52dbc0cac6af73dd
emmakuen <emmaku@protonmail.com>
2022-05-21 19:22:09 +0900

    add smooth scrolling

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
+// Update year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
 ///////////////////////////////////////////
 // Mobile navigation
-
 const buttonNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 buttonNavEl.addEventListener("click", () => {
