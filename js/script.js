3fe22298c2f4f6468af997031994d5df4f19e952
emmakuen <emmaku@protonmail.com>
2022-05-21 18:16:11 +0900

    make mobile navigation functional

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+///////////////////////////////////////////
+// Mobile navigation
+
+const buttonNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+buttonNavEl.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
+});
